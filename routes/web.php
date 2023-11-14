c58bd063689cf6c4441ffdd1b23ad8e2e27bc9dd
Nabe <vohongduc000@gmail.com>
2023-11-14 16:07:58 +0700

    fix loi dashboard

--- routes/web.php
+++ routes/web.php
@@ -21,15 +21,15 @@ use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 Route::get('/', [HomeController::class,'index'])->name('home');
 
-// Route::middleware([
-//     'auth:sanctum',
-//     config('jetstream.auth_session'),
-//     'verified',
-// ])->group(function () {
-//     Route::get('/dashboard', function () {
-//         return view('dashboard');
-//     })->name('dashboard');
-// });
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
 
 // Verify
 
